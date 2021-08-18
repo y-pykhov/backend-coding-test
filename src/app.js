747c86e513e33f81b5359d1538baf89a9c0e4fb8
Yevhenii Pykhov <y.pykhov@aimprosoft.com>
2021-08-18 17:24:17 +0300

    fix eslint errors

--- src/app.js
+++ src/app.js
@@ -30,46 +30,46 @@ module.exports = (db) => {
         if (startLatitude < -90 || startLatitude > 90 || startLongitude < -180 || startLongitude > 180) {
             return res.send({
                 error_code: 'VALIDATION_ERROR',
-                message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+                message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
             });
         }
 
         if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
             return res.send({
                 error_code: 'VALIDATION_ERROR',
-                message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+                message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
             });
         }
 
         if (typeof riderName !== 'string' || riderName.length < 1) {
             return res.send({
                 error_code: 'VALIDATION_ERROR',
-                message: 'Rider name must be a non empty string'
+                message: 'Rider name must be a non empty string',
             });
         }
 
         if (typeof driverName !== 'string' || driverName.length < 1) {
             return res.send({
                 error_code: 'VALIDATION_ERROR',
-                message: 'Rider name must be a non empty string'
+                message: 'Rider name must be a non empty string',
             });
         }
 
         if (typeof driverVehicle !== 'string' || driverVehicle.length < 1) {
             return res.send({
                 error_code: 'VALIDATION_ERROR',
-                message: 'Rider name must be a non empty string'
+                message: 'Rider name must be a non empty string',
             });
         }
 
-        var values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
+        const values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
 
-        const result = db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
+        db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
             if (err) {
                 logger.error(err);
                 return res.send({
                     error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
+                    message: 'Unknown error',
                 });
             }
 
@@ -78,7 +78,7 @@ module.exports = (db) => {
                     logger.error(err);
                     return res.send({
                         error_code: 'SERVER_ERROR',
-                        message: 'Unknown error'
+                        message: 'Unknown error',
                     });
                 }
 
@@ -93,14 +93,14 @@ module.exports = (db) => {
                 logger.error(err);
                 return res.send({
                     error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
+                    message: 'Unknown error',
                 });
             }
 
             if (rows.length === 0) {
                 return res.send({
                     error_code: 'RIDES_NOT_FOUND_ERROR',
-                    message: 'Could not find any rides'
+                    message: 'Could not find any rides',
                 });
             }
 
@@ -114,14 +114,14 @@ module.exports = (db) => {
                 logger.error(err);
                 return res.send({
                     error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
+                    message: 'Unknown error',
                 });
             }
 
             if (rows.length === 0) {
                 return res.send({
                     error_code: 'RIDES_NOT_FOUND_ERROR',
-                    message: 'Could not find any rides'
+                    message: 'Could not find any rides',
                 });
             }
 
@@ -135,7 +135,7 @@ module.exports = (db) => {
                 logger.error(err);
                 return res.send({
                     error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
+                    message: 'Unknown error',
                 });
             }
 
